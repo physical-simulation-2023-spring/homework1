8d0d4c0801f0e8878b1b72ae99481e0e8af347cd
Combolv <lkb19@mails.tsinghua.edu.cn>
2023-02-19 17:32:17 +0800

    Finish Update Pose.

--- core.py
+++ core.py
@@ -1,3 +1,24 @@
 import taichi as ti
-ti.init()
+import taichi.math as tm
+import numpy as np
+ti.init(arch=ti.cpu)
 
+link_list = []
+joint_list = []
+
+@ti.func
+def AddLink(link_name):
+    if link_name == "hinge":
+        pass
+    elif link_name == "?":
+        pass
+
+
+@ti.func
+def AddJoint():
+    pass
+
+
+@ti.func
+def Not():
+    pass
--- double_pendulum_2d.py
+++ double_pendulum_2d.py
@@ -4,9 +4,9 @@ import numpy as np
 ti.init(arch=ti.cpu)
 
 num_links = 2
-pendulum_length = 1.
-ball_radius = 0.2
-ball_mass = 1.
+pendulum_length = 2. / num_links
+ball_radius = 0.2 * pendulum_length
+ball_mass = 25. * ball_radius ** 2 
 ball_inertia = 0.5 * ball_mass * ball_radius ** 2
 
 position = ti.Vector.field(2, dtype=float, shape=(num_links, ))
@@ -20,16 +20,41 @@ generalized_velocity = ti.Vector([0., 0.])
 mass_matrix = ti.Matrix(np.diag([ball_mass] * (num_links * 2) + [ball_inertia] * num_links))
 
 jacobian = ti.Matrix([[0.] * num_links] * (num_links * 3))
+is_angular_jacobian_initialized = False
 jacobian_time_derivative = ti.Matrix([[0.] * num_links] * (num_links * 3))
 
 @ti.func
+def get_rotation_mat(q):
+    return ti.Matrix([[tm.cos(q), tm.sin(q)], [-tm.sin(q), tm.cos(q)]])
+
+@ti.func
 def UpdateLinkPose():
+    ti.loop_config(serialize=True)
     for i in rotation_matrices:
-        pass
+        if i == 0:
+            rotation_matrices[i] = get_rotation_mat(generalized_position[i])
+            position[i] = rotation_matrices[i] @ ti.Vector([0., -pendulum_length])
+        else:
+            rotation_matrices[i] = rotation_matrices[i - 1] @ get_rotation_mat(generalized_position(i))
+            position[i] = rotation_matrices[i] @ ti.Vector([0., -pendulum_length]) + position[i - 1]
 
 @ti.func
 def ComputeJacobian():
-    pass
+    # In our case, the angular jacobian is constant.
+    if not is_angular_jacobian_initialized:
+        for i in range(num_links):
+            for j in range(num_links):
+                if i <= j:
+                    jacobian[num_links * 2 + i, j] = 1
+                else:
+                    jacobian[num_links * 2 + i, j] = 0
+        is_angular_jacobian_initialized = True
+    # Then we compute the linear Jacobian.
+    ti.loop_config(serialize=True)
+    for i in range(num_links):
+        for j in range(2):
+            for k in range(num_links):
+                jacobian[2 * i + j, k] = ...
 
 @ti.func
 def ComputeJacobianTimeDerivative():
@@ -37,17 +62,17 @@ def ComputeJacobianTimeDerivative():
 
 @ti.func
 def ComputeGeneralizedForce():
-    Q = jacobian.T @ external_force_and_torque
+    Q = jacobian.transpose() @ external_force_and_torque
     return Q
     
 @ti.func
 def ComputeCoriolisTerm():
-    C = jacobian.T @ mass_matrix @ jacobian_time_derivative @ generalized_velocity
+    C = jacobian.transpose() @ mass_matrix @ jacobian_time_derivative @ generalized_velocity
     return C
 
 @ti.func
 def ComputeGeneralizedMass():
-    M = jacobian.T @ mass_matrix @ jacobian
+    M = jacobian.transpose() @ mass_matrix @ jacobian
     return M
 
 @ti.func
